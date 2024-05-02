b0772e0deaee5cb0ffd80159c7bfc29eade11b2d
Atul Dubal <atuldubal199@gmail.com>
2024-05-02 12:30:15 +0530

    Fronted

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,11 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Banner from '../components/Banner'
 import '../index.css'
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 function Home() {
   return (
     <React.Fragment>
+      <SpeedInsights/>
       <Navbar />
       <Banner/>
       <hr/>
