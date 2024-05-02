3ef3460958ece2ae80bda803c6b93cc6a8d7280d
Atul Dubal <atuldubal199@gmail.com>
2024-05-02 10:51:37 +0530

    Fronted

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -40,13 +40,13 @@ function ContactUs() {
         <div className='flex flex-col text-center space-y-3 pt-10'>
           <h1 className='text-xl font-bold '>Connect Us On </h1>
           <ol className='flex flex-row space-x-5 justify-center'>
-          <button className=" rounded-md bg-[#E1306C] hover:opacity-90 duration-200 px-2 py-2 text-white  text-sm"><FontAwesomeIcon size='xl' icon={faInstagram} /> Instagram</button>
+          <button className=" rounded-md bg-[#E1306C] hover:opacity-90 duration-200 px-2 py-2 text-white  text-[10px]  md:text-sm"><FontAwesomeIcon size='xl' icon={faInstagram} /> Instagram</button>
                           
-                          <button className=" rounded-md bg-blue-700 hover:bg-blue-900 duration-200 px-2 py-2 text-white  text-sm"><FontAwesomeIcon size='xl' icon={faLinkedin} /> Linkdin</button>
+                          <button className=" rounded-md bg-blue-700 hover:bg-blue-900 duration-200 px-2 py-2 text-white  text-[10px]  md:text-sm"><FontAwesomeIcon size='xl' icon={faLinkedin} /> Linkdin</button>
                    
-                          <button className=" rounded-md bg-black hover:bg-slate-900 duration-200 px-2 py-2 text-white  text-sm"><FontAwesomeIcon size='xl' icon={faGithub} /> GitHub</button>
+                          <button className=" rounded-md bg-black hover:bg-slate-900 duration-200 px-2 py-2 text-white  text-[10px]  md:text-sm"><FontAwesomeIcon size='xl' icon={faGithub} /> GitHub</button>
                    
-                          <button className=" rounded-md bg-red-600 hover:bg-red-700 duration-200 px-2 py-2 text-white  text-sm"><FontAwesomeIcon size='xl' icon={faYoutube} /> YouTube</button>
+                          <button className=" rounded-md bg-red-600 hover:bg-red-700 duration-200 px-2 py-2 text-white  text-[10px]  md:text-sm"><FontAwesomeIcon size='xl' icon={faYoutube} /> YouTube</button>
                           
           </ol>
         </div>
