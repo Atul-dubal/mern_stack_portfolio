289391556c9e61c3f8fb37df8bd1383fd44b7c1f
Atul Dubal <atuldubal199@gmail.com>
2024-05-02 12:27:49 +0530

    Fronted

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,7 +4,7 @@ import { Carousel } from 'react-responsive-carousel'
 function ProjectCard() {
     return (
         <React.Fragment>
-            <div className='flex flex-col md:flex-row justify-start w-[90%] my-5 mx-auto bg-slate-200 border space-y-3 shadow-xl space-x-3 rounded-xl p-3 hover:scale-105 duration-700'>
+            <div className='flex flex-col md:flex-row  w-[90%] my-5 mx-auto bg-slate-200 border space-y-3 shadow-xl space-x-3 rounded-xl p-3 hover:scale-105 duration-700'>
                 <div className=' w-[100%] md:w-[60%] flex flex-col justify-self-start space-y-4 '>
                     <div className='flex flex-row items-center space-x-4'>
                         <img className=' w-20 rounded-xl border-black' src='/js_logo.png' alt='' />
@@ -13,13 +13,13 @@ function ProjectCard() {
                     <p className='text-md font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati expedita iusto, corrupti facere, voluptatibus harum velit praesentium temporibus odit deserunt quo autem commodi porro dicta vero, saepe libero nisi alias.</p>
                     <button className='px-2 py-2 rounded-md shadow-lg text-white  bg-blue-600 border-none w-56'>See Project Source Code</button>
                 </div>
-                <div className='justify-center w-[100%] md:w-[40%]'>
-                <Carousel infiniteLoop={true} useKeyboardArrows={true} showIndicators={false} dynamicHeight={false} autoPlay={true} showStatus={false} showThumbs={false}>
-                    <div className='md:w-[80%] flex justify-center flex-row'>
-                        <img className='w-[50%] rounded-lg border-black' src='/about_us.jpg' alt='' />
+                <div className='flex justify-center mx-auto w-[90%] md:w-[40%]'>
+                <Carousel infiniteLoop={true} showIndicators={false}  autoPlay={true} showStatus={true} showThumbs={false}>
+                    <div className='md:w-[90%] mx-auto flex justify-center flex-row'>
+                        <img className='w-[100%] rounded-lg border-black' src='/about_us.jpg' alt='' />
                     </div>
-                    <div className='md:w-[80%]'>
-                        <img className='w-[50%] rounded-lg border-black' src='/php.png' alt='' />
+                    <div className='md:w-[100%]'>
+                        <img className='w-[100%] rounded-lg border-black' src='/contact_us.jpg' alt='' />
                     </div>
                 </Carousel>
             </div>
