90b569e6b96048ceb476043c7e06be097a05be37
Atul Dubal <atuldubal199@gmail.com>
2024-05-02 11:26:54 +0530

    Fronted

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function ProjectCard() {
+    return (
+        <React.Fragment>
+            <div className='flex flex-col md:flex-row justify-start w-[90%] m-5 bg-slate-200 border shadow-xl space-x-3 rounded-xl p-3 hover:scale-105 duration-700'>
+                <div className=' w-[100%] md:w-[50%] flex flex-col justify-self-start space-y-4 '>
+                    <div className='flex flex-row items-center space-x-4'>
+                        <img className=' w-20 rounded-xl border-black' src='/js_logo.png' alt='' />
+                        <p className='text-center text-xl '>Project name</p>
+                    </div>
+                    <p className='text-md font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati expedita iusto, corrupti facere, voluptatibus harum velit praesentium temporibus odit deserunt quo autem commodi porro dicta vero, saepe libero nisi alias.</p>
+                    <button className='btn w-56'>See Project Source Code</button>
+                </div>
+                <div className='justify-self-end w-[100%] md:w-[50%]'>
+
+                    <img className='w-[80%] rounded-lg border-black' src='/about_us.jpg' alt='' />
+
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default ProjectCard
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,14 +2,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import ProjectCard from '../components/ProjectCard'
 
 function Projects() {
   return (
     <>
       <Navbar />
-      <div className='mt-[60px] py-[20px] bg-white text-black'>
+      <div className='mt-[60px] py-[20px] bg-white text-black max-w-screen-lg mx-auto container'>
         <h1 className='text-center font-bold text-2xl my-5'>Projects</h1>
-      
+        <ProjectCard type='BE ECS' school='Pillai HOC Collage of Engineering & Technology, Rasayni, 410207' marks='CGPA : 8.5' />
+        <ProjectCard type='BE ECS' school='Pillai HOC Collage of Engineering & Technology, Rasayni, 410207' marks='CGPA : 8.5' />
+
       </div>
       <Footer />
     </>
