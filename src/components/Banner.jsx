f0cbeae44386178d253ad0ce77d5a0e88947d775
Atul Dubal <atuldubal199@gmail.com>
2024-05-02 10:31:14 +0530

    Fronted

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,58 +1,59 @@
 import React from 'react'
 import { ReactTyped } from "react-typed";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faYoutube , faInstagram , faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faYoutube, faInstagram, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 function Banner() {
     return (
         <React.Fragment>
-            <div className='max-w-screen-xl mt-[60px]  flex container mx-auto justify-center space-x-5  bg-white text-black'>
-                <div className='flex  flex-col p-6 mt-[30px] justify-between md:flex-row   '>
-                    {/* Left Side */}
-                    <div className='md:w-[60%] text-center space-y-6 order-2 md:order-1 md:mt-[48px]'>
-                        <h1 className='text-pink-500 text-center font-bold text-2xl'>Welcome To My Portfolio </h1>
-                        <p className='text-center text-2xl font-bold'>Hii, I am 
-                            <ReactTyped
-                                loop='true'
-                                backSpeed={70}
-                                typeSpeed={70}
-                                className='text-pink-500 text-center text-2xl font-bold px-2'
-                                strings={['Atul D. Dubal', 'Programmer', 'Web Developer', 'Android Developer', 'Coder', 'YouTuber']}
-                            />
-                        </p>
+            <div className=' max-w-screen-2xl mx-auto container mt-[60px] bg-white text-black'>
+                <div className='   flex   justify-center space-x-5  '>
+                    <div className='flex  flex-col p-6 mt-[30px] justify-between md:flex-row   '>
+                        {/* Left Side */}
+                        <div className='md:w-[60%] text-center space-y-6 order-2 md:order-1 md:mt-[48px]'>
+                            <h1 className='text-pink-500 text-center font-bold text-2xl'>Welcome To My Portfolio </h1>
+                            <p className='text-center text-2xl font-bold'>Hii, I am
+                                <ReactTyped
+                                    loop='true'
+                                    backSpeed={70}
+                                    typeSpeed={70}
+                                    className='text-pink-500 text-center text-2xl font-bold px-2'
+                                    strings={['Atul D. Dubal', 'Programmer', 'Web Developer', 'Android Developer', 'Coder', 'YouTuber']}
+                                />
+                            </p>
 
-                        <p className=' font-semibold text-start'>Web Development, Android Development, and languages like Python, Java, and PHP are where I thrive. Exploring these fields keeps me engaged in the ever-evolving world of technology. As an intern at Prodigy InfoTech and a former Android Developer Intern at NullClass, I&apos;ve gained solid experience in Android and Web Development, along with proficiency in Python and Java programming.</p>
-                        {/* Social Icons */}
-                        <div className='flex flex-col  space-y-3'>
-                            <p className='text-left font-bold text-xl text-black'>Available On </p>
-                            <ol className=' flex flex-row space-x-5'>
-                                {/* <li><FontAwesomeIcon size='xl' icon={faInstagram}/></li>
+                            <p className=' font-semibold text-start'>Web Development, Android Development, and languages like Python, Java, and PHP are where I thrive. Exploring these fields keeps me engaged in the ever-evolving world of technology. As an intern at Prodigy InfoTech and a former Android Developer Intern at NullClass, I&apos;ve gained solid experience in Android and Web Development, along with proficiency in Python and Java programming.</p>
+                            {/* Social Icons */}
+                            <div className='flex flex-col  space-y-3'>
+                                <p className='text-left font-bold text-xl text-black'>Available On </p>
+                                <ol className=' flex flex-row space-x-5'>
+                                    {/* <li><FontAwesomeIcon size='xl' icon={faInstagram}/></li>
                                 <li><FontAwesomeIcon size='xl' icon={faLinkedin}/></li>
                                 <li><FontAwesomeIcon size='xl' icon={faYoutube}/></li>
                                 <li><FontAwesomeIcon size='xl' icon={faGithub}/></li> */}
-                                 
-                                 <button className=" rounded-md bg-[#E1306C] hover:opacity-90 duration-200 px-2 py-2 text-white  text-sm"><FontAwesomeIcon size='xl' icon={faInstagram} /> Instagram</button>
-                          
-                                 <button className=" rounded-md bg-blue-700 hover:bg-blue-900 duration-200 px-2 py-2 text-white  text-sm"><FontAwesomeIcon size='xl' icon={faLinkedin} /> Linkdin</button>
-                          
-                                 <button className=" rounded-md bg-black hover:bg-slate-900 duration-200 px-2 py-2 text-white  text-sm"><FontAwesomeIcon size='xl' icon={faGithub} /> GitHub</button>
-                          
-                                 <button className=" rounded-md bg-red-600 hover:bg-red-700 duration-200 px-2 py-2 text-white  text-sm"><FontAwesomeIcon size='xl' icon={faYoutube} /> YouTube</button>
-                                 </ol>
+
+                                    <button className=" rounded-md bg-[#E1306C] hover:opacity-90 duration-200 px-2 py-2 text-white  text-sm"><FontAwesomeIcon size='xl' icon={faInstagram} /> Instagram</button>
+
+                                    <button className=" rounded-md bg-blue-700 hover:bg-blue-900 duration-200 px-2 py-2 text-white  text-sm"><FontAwesomeIcon size='xl' icon={faLinkedin} /> Linkdin</button>
+
+                                    <button className=" rounded-md bg-black hover:bg-slate-900 duration-200 px-2 py-2 text-white  text-sm"><FontAwesomeIcon size='xl' icon={faGithub} /> GitHub</button>
+
+                                    <button className=" rounded-md bg-red-600 hover:bg-red-700 duration-200 px-2 py-2 text-white  text-sm"><FontAwesomeIcon size='xl' icon={faYoutube} /> YouTube</button>
+                                </ol>
+                            </div>
+
+                            {/* Email Input */}
+                            <label className="input  border-black bg-white text-black flex items-center gap-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
+                                <input type="text" className="grow outline-black" placeholder="Email" />
+                            </label>
+                            <button className='bg-pink-600 px-4 py-2 flex start-0 rounded-md hover:bg-pink-300 duration-500 ease-in-out'>Lets Connect</button>
                         </div>
-
-                        {/* Email Input */}
-                        <label className="input  border-black bg-white text-black flex items-center gap-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
-                            <input type="text" className="grow outline-black" placeholder="Email" />
-                        </label>
-                        <button className='bg-pink-600 px-4 py-2 flex start-0 rounded-md hover:bg-pink-300 duration-500 ease-in-out'>Lets Connect</button>
+                        {/* Right Side */}
+                        <div className=' order-1 md:order-2 '>
+                            <img className='w-[350px] md:w-[500px] hover:scale-110 duration-300' src='/developer_img.jpg' alt='' />
+                        </div>
                     </div>
-                    {/* Right Side */}
-                    <div className=' order-1 md:order-2 '>
-                        <img className='w-[350px] md:w-[500px] hover:scale-110 duration-300'  src='/developer_img.jpg' alt='' />
-                    </div>
-
                 </div>
             </div>
         </React.Fragment>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,31 +18,30 @@ const Navbar = () => {
     }
     return (
         <React.Fragment>
-            <div className="navbar  z-50 text-black max-w-[100%] md:px-[150px] container mx-auto fixed top-0 left-0 right-0 shadow-sm bg-slate-200">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            <div className=' max-w-screen-2xl z-50 md:px-[150px] mx-auto container  fixed top-0 left-0 right-0 shadow-sm bg-slate-200 text-black'>
+                <div className="navbar">
+                    <div className="navbar-start">
+                        <div className="dropdown">
+                            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            </div>
+                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow  rounded-box w-52">
+                                <Navbar_Items />
+                            </ul>
                         </div>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow  rounded-box w-52">
-                            <Navbar_Items />
-                        </ul>
+                        <a className="text-2xl cursor-pointer font-bold">Atul<span className='text-pink-500'>Dubal</span> </a>
                     </div>
-                    <a className="text-2xl cursor-pointer font-bold">Atul<span className='text-pink-500'>Dubal</span> </a>
-                </div>
-                <div className="navbar-end w-[75%] space-x-4">
-                    <div className="navbar-center hidden lg:flex">
-                        <ul className="menu menu-horizontal px-1 space-x-6">
-                            <Navbar_Items />
-                        </ul>
+                    <div className="navbar-end w-[75%] space-x-4">
+                        <div className="navbar-center hidden lg:flex">
+                            <ul className="menu menu-horizontal px-1 space-x-6">
+                                <Navbar_Items />
+                            </ul>
+                        </div>
+
+                        <button className=" rounded-md md:flex bg-[#E1306C] hover:opacity-90 duration-200 px-2 py-2 text-white  text-sm"><FontAwesomeIcon icon={faInstagram} /> Instagram</button>
+
+                        <button className=" rounded-md hidden md:flex bg-blue-700 hover:bg-blue-900 duration-200 px-2 py-2 text-white  text-sm"><FontAwesomeIcon icon={faLinkedin} /> Linkdin</button>
                     </div>
-
-                    {/* <button className=" rounded-md hidden md:flex bg-[#E1306C] hover:opacity-90 duration-200 px-2 py-2 text-white  text-sm"><FontAwesomeIcon icon={faInstagram} /> Instagram</button>
-
-                    <button className=" rounded-md hidden md:flex bg-blue-700 hover:bg-blue-900 duration-200 px-2 py-2 text-white  text-sm"><FontAwesomeIcon icon={faLinkedin} /> Linkdin</button> */}
-
-
-
                 </div>
             </div>
         </React.Fragment>
