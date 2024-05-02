7377bc7be2e4c9291ec4038930c177307d882841
Atul Dubal <atuldubal199@gmail.com>
2024-05-02 12:45:52 +0530

    Fronted

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,12 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Banner from '../components/Banner'
 import '../index.css'
+import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/react"
 function Home() {
   return (
     <React.Fragment>
+      <Analytics/>
       <SpeedInsights/>
       <Navbar />
       <Banner/>
