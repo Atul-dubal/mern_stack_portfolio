cb51be2a5077ca654d401487e452c15c30395c68
Atul Dattatray Dubal <80565891+Atul-dubal@users.noreply.github.com>
2024-06-05 22:24:57 +0530

    Update EducationCard.jsx

--- src/components/EducationCard.jsx
+++ src/components/EducationCard.jsx
@@ -5,7 +5,7 @@ function EducationCard(prop) {
         <React.Fragment>
 
                 <div className="card card-side mx-3 h-[200px]  m-5 shadow-2xl hover:scale-105 duration-300">
-                    <figure><img className='w-[280px]' src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" /></figure>
+                    <figure><img className='w-[280px]' src="/images/books.png" alt="Movie" /></figure>
                     <div className="card-body">
                         <h2 className="card-title">{prop.type}</h2>
                         <p className=' text-sm'>{prop.school}</p>
